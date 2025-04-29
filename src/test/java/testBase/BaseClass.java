d8f5b0d11e4d74e20769a1a5c16fd9c50b594eab
sandeep devadiga <sandeepdevadiga721@gmail.com>
2025-04-29 17:58:27 +0530

    commiting changes in baseclass for remote execution and update in report

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -3,6 +3,7 @@ package testBase;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Date;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
@@ -11,11 +12,14 @@ import java.util.Random;
 import java.util.UUID;
 
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
@@ -32,23 +36,50 @@ public class BaseClass {
 		FileReader file = new FileReader("./src//test//resources//config.Properties");
 		p = new Properties();
 		p.load(file);
-
-		switch (br.toLowerCase()) {
-
-		case "chrome":
-			driver = new ChromeDriver();
-			break;
-		case "edge":
-			driver = new EdgeDriver();
-			break;
-		case "firefox":
-			driver = new FirefoxDriver();
-			break;
-		default:
-			System.out.println("Invalid browser name...");
-			return;
+		
+		if(p.getProperty("execution_env").equalsIgnoreCase("remote"))
+		{
+			DesiredCapabilities capabilites=new DesiredCapabilities();
+			
+			//OS
+			switch (os.toLowerCase())
+			{
+			case "windows":capabilites.setPlatform(Platform.WIN10);break;
+			case "mac":capabilites.setPlatform(Platform.MAC);break;
+			case "linux":capabilites.setPlatform(Platform.LINUX);break;
+			default:System.out.println("No Matching OS");return;
+			}
+			//browser
+			switch (br.toLowerCase())
+			{
+			case "chrome":capabilites.setBrowserName("chrome");break;
+			case "edge":capabilites.setBrowserName("MicrosoftEdge");break;
+			case "firefox":capabilites.setBrowserName("firefox");break;
+			default:System.out.println("No Matching Browser");return;
+			}
+			
+			driver=new RemoteWebDriver(new URL("http://localhost:4444"),capabilites);
 		}
+		
+		if(p.getProperty("execution_env").equalsIgnoreCase("local"))
+		{
+			switch (br.toLowerCase()) {
 
+			case "chrome":
+				driver = new ChromeDriver();
+				break;
+			case "edge":
+				driver = new EdgeDriver();
+				break;
+			case "firefox":
+				driver = new FirefoxDriver();
+				break;
+			default:
+				System.out.println("Invalid browser name...");
+				return;
+			}
+		}
+		
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
